8461ecd924d254bcb4b3f4b690079a45dff1fa82
Shaun-B1 <shaun.macwilliam@gmail.com>
2020-06-08 21:31:36 +1000

    reset button has been added

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function makeOriginalGrid() {
   for ( i = 0; i < 256; i++ ) {
     grid = document.createElement('div');
 
-    grid.classList.add('blanksquare');
+    grid.classList.add('whitesquare');
     gameBoard.appendChild(grid);
     gameBoard.addEventListener('mouseover', hoverBlack);
   }
@@ -15,8 +15,46 @@ function makeOriginalGrid() {
 makeOriginalGrid();
 
 function hoverBlack(e) {
-  if (e.target.className === 'blanksquare') {
-    e.target.classList.remove('blanksquare')
+  if (e.target.className === 'whitesquare') {
+    e.target.classList.remove('whitesquare')
     e.target.classList.add('blacksquare')
   }
 }
+
+function newGrid() {
+
+  function resetGameBoard() {
+    return prompt('How big would you like the new board to be?')
+  }
+
+  let newGameBoard = resetGameBoard();
+
+  if (isNaN(newGameBoard)) {
+    alert('You didn\'t enter a number!')
+  } else if (newGameBoard == null || newGameBoard == '' || newGameBoard.trim() == '') {
+    alert('You didn\'t enter a number!')
+  } else {
+    while (gameBoard.hasChildNodes()) {
+      gameBoard.removeChild(gameBoard.firstChild);
+      gameBoard.removeAttribute('id');
+      gameBoard.setAttribute('id', 'gameBoard2');
+    }
+
+    for ( i = 0; i < newGameBoard * newGameBoard; i++ ) {
+      const grid = document.createElement('div');
+      grid.classList.add('whitesquare');
+
+      let gameBoard2 = document.getElementById('gameBoard2')
+      gameBoard2.style.gridTemplateColumns = `repeat(${newGameBoard}, 1fr)`;
+      gameBoard2.style.gridTemplateRows = `repeat(${newGameBoard}, 1fr)`;
+      gameBoard2.appendChild(grid);
+
+      gameBoard2.addEventListener('mouseover', hoverBlack);
+    }
+  }
+}
+
+const reset = document.querySelector('#reset')
+reset.addEventListener('click', (e) => {
+  newGrid()
+})
