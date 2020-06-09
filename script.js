4109f11b4d8e648b05c5bf8a8f96be2c9d3b977b
Shaun-B1 <shaun.macwilliam@gmail.com>
2020-06-09 21:21:51 +1000

    Finished project

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 let gameBoard = document.querySelector('#gameBoard');
 let grid;
 
+function hoverBlack(e) {
+  if (e.target.className === 'blankSquare') {
+    e.target.style.background = 'black';
+    e.target.style.opacity = 1;
+  }
+}
+
+function hoverGreyscale(e) {
+  if (e.target.className == 'blankSquare') {
+    if (e.target.style.opacity < 1 && e.target.style.backgroundColor != 'black') {
+      e.target.style.backgroundColor = 'black';
+      e.target.style.opacity = (parseFloat(e.target.style.opacity) || 0) + 0.1;
+    } else if (e.target.style.opacity == 1 & e.target.style.backgroundColor != 'black') {
+      e.target.style.backgroundColor = 'black';
+      e.target.style.opacity = 0.1;
+    } else if (e.target.style.opacity < 1 && e.target.style.backgroundColor == 'black') {
+      e.target.style.opacity = (parseFloat(e.target.style.opacity) || 0) + 0.1;
+    } else if (e.target.style.opacity == 1 && e.target.style.backgroundColor == 'black') {
+      return;
+    }
+  }
+}
+
+function generateRandomColor()
+{
+    let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
+
+function hoverMulticolour(e) {
+  if (e.target.className === 'blankSquare') {
+    e.target.style.background = generateRandomColor();
+    e.target.style.opacity = 1;
+  }
+}
 
 function makeOriginalGrid() {
   for ( i = 0; i < 256; i++ ) {
     grid = document.createElement('div');
 
-    grid.classList.add('whitesquare');
+    grid.classList.add('blankSquare');
     gameBoard.appendChild(grid);
     gameBoard.addEventListener('mouseover', hoverBlack);
   }
@@ -14,17 +49,10 @@ function makeOriginalGrid() {
 
 makeOriginalGrid();
 
-function hoverBlack(e) {
-  if (e.target.className === 'whitesquare') {
-    e.target.classList.remove('whitesquare')
-    e.target.classList.add('blacksquare')
-  }
-}
-
 function newGrid() {
 
   function resetGameBoard() {
-    return prompt('How big would you like the new board to be?')
+    return prompt('How big would you like the new board to be?', '')
   }
 
   let newGameBoard = resetGameBoard();
@@ -42,7 +70,7 @@ function newGrid() {
 
     for ( i = 0; i < newGameBoard * newGameBoard; i++ ) {
       const grid = document.createElement('div');
-      grid.classList.add('whitesquare');
+      grid.classList.add('blankSquare');
 
       let gameBoard2 = document.getElementById('gameBoard2')
       gameBoard2.style.gridTemplateColumns = `repeat(${newGameBoard}, 1fr)`;
@@ -57,4 +85,25 @@ function newGrid() {
 const reset = document.querySelector('#reset')
 reset.addEventListener('click', (e) => {
   newGrid()
+})
+
+const blackout = document.querySelector('#blackout')
+blackout.addEventListener('click', (e) => {
+  gameBoard.removeEventListener('mouseover', hoverGreyscale)
+  gameBoard.removeEventListener('mouseover', hoverMulticolour)
+  gameBoard.addEventListener('mouseover', hoverBlack)
+})
+
+const greyscale = document.querySelector('#greyscale')
+greyscale.addEventListener('click', (e) => {
+  gameBoard.removeEventListener('mouseover', hoverBlack)
+  gameBoard.removeEventListener('mouseover', hoverMulticolour)
+  gameBoard.addEventListener('mouseover', hoverGreyscale)
+})
+
+const multicolour = document.querySelector('#multicolour')
+multicolour.addEventListener('click', (e) => {
+  gameBoard.removeEventListener('mouseover', hoverGreyscale)
+  gameBoard.removeEventListener('mouseover', hoverBlack)
+  gameBoard.addEventListener('mouseover', hoverMulticolour)
 })
